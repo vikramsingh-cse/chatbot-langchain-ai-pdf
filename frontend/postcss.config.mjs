45899f2ea6db3677a95864a3f67af69fb7d692a7
Vikram Singh <vikrams83048@gmail.com>
2026-02-01 10:40:58 +0530

    Update postcss.config.mjs

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
   },
 };
-
-export default config;
